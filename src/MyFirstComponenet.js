916fe11bcf8f83fbb8a64b8105503458b97ca0e7
Rawan <Rawanibraheem385@gmail.com>
2025-02-03 12:31:58 +0300

    use condition with classes

--- src/MyFirstComponenet.js
+++ src/MyFirstComponenet.js
@@ -5,11 +5,15 @@ export default function MyFirstComponent(){
         color:"red",
         fontSize:"15px",
     }
+    const person ={
+        name :"Ali",
+    }
     return(
         <div>
             <h1 className={"changeColor"}>Hello my first rect app</h1>
             <p style={{fontSize:"20px", backgroundColor:"white", color:"black"}}>Generate Lorem Ipsum placeholder text in any number of characters, words, sentences or paragraphs. Learn about the origins of the passage and its history, from the Roman era to to</p>
             <button style={button}>Click here</button>
+            <p className={person.name=="Ali" ? "colorAli" : "colorName"}>Name</p>
         </div>
     );
 }
