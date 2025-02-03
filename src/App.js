687db9ae95b1d854f4e0472ac8d31403ef576b81
Rawan <Rawanibraheem385@gmail.com>
2025-02-03 14:20:41 +0300

    work with props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
 import Artical from './Artical';
 function App() {
+  const firstContent="Lorem ipsum Lorem ipsum 11 "
+  const secondContent="Lorem ipsum Lorem ipsum 22 "
   return (
     <div className="App">
       <header className="App-header">
-        <Artical name="Ali" email="Ali@gmail.com"/>
-        <Artical name="Ahmad" email="Ahmad@gmail.com"/>
+        <Artical name="Ali" email="Ali@gmail.com" content={firstContent}/>
+        <Artical name="Ahmad" email="Ahmad@gmail.com"  content={secondContent}/>
         <Artical name="Nour" email="Nour@gmail.com"/>
-        <Artical name="Rawan" email="Rawan@gmail.com"/>
+        <Artical name="Rawan" email="rawan@gmail.com">
+          <p style={{color:"red"}}>Lorem ipsum Lorem ipsum</p>
+        </Artical>
+        <Artical name="Nour22" email="Nour22@gmail.com">
+          <p style={{color:"blue"}}>Lorem ipsum Lorem ipsum</p>
+        </Artical>
       </header>
     </div>
   );
--- src/Artical.js
+++ src/Artical.js
@@ -1,8 +1,10 @@
-export default function Artical({name, email}) {
+export default function Artical({name, email, content="No Content Yet", children}) {
     return (
         <div>
             <h3>{name}</h3>
             <h3>{email}</h3>
+            <hr />
+            <p>{children}</p>
         </div>
     );
 }
