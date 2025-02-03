7f70fd821e7864d6cb73d0b914e00eddd98ad39e
Rawan <Rawanibraheem385@gmail.com>
2025-02-03 12:23:35 +0300

    JSX

--- src/MyFirstComponenet.js
+++ src/MyFirstComponenet.js
@@ -1,7 +1,15 @@
 import './MyFirstComponent.css';
 
 export default function MyFirstComponent(){
+    const button ={
+        color:"red",
+        fontSize:"15px",
+    }
     return(
-        <h1>Hello my first rect app</h1>
+        <div>
+            <h1 className={"changeColor"}>Hello my first rect app</h1>
+            <p style={{fontSize:"20px", backgroundColor:"white", color:"black"}}>Generate Lorem Ipsum placeholder text in any number of characters, words, sentences or paragraphs. Learn about the origins of the passage and its history, from the Roman era to to</p>
+            <button style={button}>Click here</button>
+        </div>
     );
 }
