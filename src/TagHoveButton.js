a8d2bdcce453797e1104e73b6c1a66ea6f5f7771
Rawan <Rawanibraheem385@gmail.com>
2025-02-04 12:55:21 +0300

    condition but in the return in the function in the div (con1 ? print1 : print2)

--- src/TagHoveButton.js
+++ src/TagHoveButton.js
@@ -1,11 +1,8 @@
 import './TestJsx.css';
-function TagHoverButton({title ,children}) {
-  if(title=="" || title==null){
-    return null;
-  }
+function TagHoverButton({ title, children }) {
   return (
     <div>
-        <button className={"hoverButton"}>{title}{children}</button>
+      {title=="" || title==null ? (<div></div>) : (<button className={"hoverButton"}>{title}{children}</button>)}
     </div>
   );
 }
