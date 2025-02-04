946c5a72a138b9910d4c4de09c54e5cc10ae6b4a
Rawan <Rawanibraheem385@gmail.com>
2025-02-04 13:19:05 +0300

    learning list rendering

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
 
 function App() {
+  const tasks=[
+    {id:1, title:"Reading"},
+    {id:2, title:"Writing"},
+    {id:3, title:"Doing your Homeworks"},
+    {id:4, title:"Sleeping"},
+    {id:4, title:"Visiting"},
+  ];
+
+  const MyTaskList = tasks.map((task)=>{
+    return <li style={{backgroundColor:"orange" ,width:"100px", padding:"20px" ,margin:"20px", color:"white", fontSize:"20px"}} key={task.id}>{task.title}</li>
+
+  });
   return (
     <div>
+      <ul>{MyTaskList}</ul>
 
     </div>
   );
