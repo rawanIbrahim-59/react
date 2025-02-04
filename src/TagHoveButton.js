7962eaaff853fc073a1981532245708db835878f
Rawan <Rawanibraheem385@gmail.com>
2025-02-04 12:56:44 +0300

    commit space only

--- src/TagHoveButton.js
+++ src/TagHoveButton.js
@@ -2,7 +2,8 @@ import './TestJsx.css';
 function TagHoverButton({ title, children }) {
   return (
     <div>
-      {title=="" || title==null ? (<div></div>) : (<button className={"hoverButton"}>{title}{children}</button>)}
+      {title=="" || title==null ? (<div></div>) : 
+      (<button className={"hoverButton"}>{title}{children}</button>)}
     </div>
   );
 }
