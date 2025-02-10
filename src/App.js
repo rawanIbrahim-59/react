d7d5ebb29e55d1e95c01adce1c4190b47e9bfa21
Rawan <Rawanibraheem385@gmail.com>
2025-02-10 14:49:48 +0300

    Arrays as State

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
-
+import {useState} from 'react';
 function App() {
-
+  const [AddDevice, setAddDevice]= useState("");
+  const [Devices, setDevices]=useState(["Mac", "iPhone", "Samsung",])
+  const deviceList = Devices.map((device)=>{
+    return <li>{device}</li>
+  })
+  function HandelClick(){
+    const newDevice = [...Devices];
+    newDevice.push(AddDevice);
+    setDevices(newDevice);
+  }
   return (
     <div style={{padding:"20px"}}>
+      {deviceList}
+      <div>
+        <input value={AddDevice} onChange={(event)=>{
+          setAddDevice(event.target.value)
+        }} />
+        <button onClick={HandelClick}>Add</button>
+      </div>
     </div>
   );
 }
